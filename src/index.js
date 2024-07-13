5c0fe940d0a18b1a3148e48a9d8fbd43286824a3
sweets-code <sweetscrave20@gmail.com>
2024-07-13 22:43:05 +1000

    added forecast html from javascript

--- src/index.js
+++ src/index.js
@@ -51,7 +51,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+         <div class="weather-forecast-icon">🌤</div>
+         <div class="weather-forecast-temperatures">
+           <div class="weather-forecast-temperature">
+            <strong>12°</strong>
+           </div>
+           <div class="weather-forecast-temperature">8°</div>
+        </div>
+     </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Melbourne");
+displayForecast();
